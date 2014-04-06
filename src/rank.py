651bd40f7d5ab1845624b1ece53701d03c8761b4
Nick Hirakawa <nh0815@gmail.com>
2014-04-06 18:20:16 -0400

    Removed whitespace.

--- src/rank.py
+++ src/rank.py
@@ -13,14 +13,14 @@ mu_values = [10, 100, 200, 500, 1000, 2000, 2500, 3000, 5000]
 
 def score_BM25(n, f, qf, r, N, dl, avdl):
 	K = compute_K(dl, avdl)
-	first = log( ( (r + 0.5) / (R - r + 0.5) ) / ( (n - r + 0.5) / (N - n - R + r + 0.5)) )
-	second = ((k1 + 1) * f) / (K + f)
-	third = ((k2+1) * qf) / (k2 + qf)
+	first = log(((r+0.5)/(R-r+0.5))/((n - r + 0.5)/(N-n-R+r+0.5)))
+	second = ((k1+1)*f)/(K+f)
+	third = ((k2+1)*qf)/(k2+qf)
 	return first * second * third
 
 
 def compute_K(dl, avdl):
-	return k1 * ((1-b) + b * (float(dl)/float(avdl)) )
+	return k1*((1-b)+b*(float(dl)/float(avdl)))
 
 
 def score_query_likelihood(f, mu, c, C, D):
