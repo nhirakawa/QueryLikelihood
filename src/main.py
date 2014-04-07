b1eeae4cfee38a48ce8f9989344528270a57b472
Nick Hirakawa <nh0815@gmail.com>
2014-04-06 20:08:14 -0400

    Added skeleton for getting results.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 __author__ = 'Nick Hirakawa'
 
 
+from parse import *
+from query import *
+import operator
+
+
 def main():
-	pass
+	qp = QueryParser(filename='../text/queries.txt')
+	cp = CorpusParser(filename='../text/corpus.txt')
+	qp.parse()
+	print 'parsing queries'
+	queries = qp.get_queries()
+	print 'parsing corpus'
+	cp.parse()
+	corpus = cp.get_corpus()
+	proc = QueryProcessor(queries, corpus, score_function='Query Likelihood')
+	print 'running queries'
+	results = proc.run()
+	print results
 
 
 if __name__ == '__main__':
+	import timeit
+	#print timeit.timeit('main()', setup='from __main__ import main', number=5)
 	main()
