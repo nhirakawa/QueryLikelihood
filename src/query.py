1bc4da17416d6bc08ff614801f729b24f71cf92f
nh0815 <nh0815@gmail.com>
2014-04-11 23:54:58 -0400

    Changed constructor of QueryProcessor. Inverted index now read from file instead of memory. Documents scored even if they don't contain query.

--- src/query.py
+++ src/query.py
@@ -8,9 +8,12 @@ import operator
 
 
 class QueryProcessor:
-	def __init__(self, queries, corpus, score_function='BM25'):
+	def __init__(self, queries, idx, dlt, ft, score_function='BM25'):
 		self.queries = queries
-		self.index, self.ft, self.dlt = build_data_structures(corpus)
+		#self.index, self.ft, self.dlt = build_data_structures(corpus)
+		self.idx_file = idx
+		self.dlt = dlt
+		self.ft = ft
 		self.score_function = score_function
 
 	def run(self):
@@ -22,9 +25,51 @@ class QueryProcessor:
 			elif self.score_function == 'Query Likelihood':
 				print 'running query %d' % qid
 				qid += 1
-				results.append(self.run_QueryLikelihood(query))
+				results.append(self.run_query_likelihood(query))
 		return results
 
+	def run_query_likelihood(self, query):
+		print 'query:', query
+		query_result = OrderedDict() # collect document rankings for each value of mu
+		for mu in mu_values:
+			print "running query with mu=%d" % mu
+			mu_result = OrderedDict() # collect document rankings for this value of mu
+			for term in query:
+				with open(self.idx_file, buffering=1000000000) as idx:
+					print 'searching index'
+					for line in idx:
+						tmp = line.split()
+						word = tmp.pop(0)
+						freq = [tuple(x.split('-')) for x in tmp]
+						if term == word:
+							print 'found word in index:', word
+							docs = set()
+
+							print 'scoring documents'
+							#score documents that contain term
+							for docid, f in freq:
+								docs.add(docid)
+								score = score_query_likelihood(f=float(f), mu=mu, c=self.ft.get_frequency(term), C=len(self.ft), D=len(self.dlt))
+								if docid in mu_result:
+									mu_result[docid] += score
+								else:
+									mu_result[docid] = score
+
+							print 'scoring other documents'
+							#score documents that don't contain term
+							tmp = [str(x) for x in range(len(self.dlt))]
+							s = set(tmp).difference(docs)
+							score = score_query_likelihood(f=0, mu=mu, c=self.ft.get_frequency(term), C=len(self.ft), D=len(self.dlt))
+							for docid in s:
+								if docid in mu_result:
+									mu_result[docid] += score
+								else:
+									mu_result[docid] = score
+							break
+			query_result[mu] = mu_result
+		return query_result
+
+'''
 	def run_BM25(self, query):
 		query_result = dict()
 		for term in query:
@@ -32,8 +77,8 @@ class QueryProcessor:
 				doc_dict = self.index[term] # retrieve index entry
 				for docid, freq in doc_dict.iteritems(): #for each document and its word frequency
 					score = score_BM25(n=len(doc_dict), f=freq, qf=1, r=0, N=len(self.dlt),
-									   dl=self.dlt.get_length(docid),
-									   avdl=self.dlt.get_average_length()) # calculate score
+					                   dl=self.dlt.get_length(docid),
+					                   avdl=self.dlt.get_average_length()) # calculate score
 					if docid in query_result: #this document has already been scored once
 						query_result[docid] += score
 					else:
@@ -72,7 +117,7 @@ class QueryProcessor:
 							mu_result[i] += score
 						else:
 							mu_result[i] = score
-			for docid in mu_result:
-				mu_result[docid] = mu_result[docid]
 			query_result[mu] = mu_result
 		return query_result
+	'''
+
